2c0806d58462bb63dc0001e86656e90880e00c6a
EgrNvk <egrnvk@gmail.com>
2026-02-13 20:26:42 +0200

    Erased model_client of input/output

--- Client/Model/model_client.py
+++ Client/Model/model_client.py
@@ -1,31 +1,21 @@
 import socket
-import time
+
 
 class Client:
     def __init__(self, host, port):
         self.host = host
         self.port = port
         self.socket = None
-        self.is_connected = False
         self.max_message_size = 1024
 
     def connect(self):
-        while True:
-            try:
-                self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.socket.connect((self.host, self.port))
-                self.is_connected = True
-                print("Connected to server")
-                break
-
-            except:
-                print("Server offline")
-                time.sleep(5)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
 
     def disconnect(self):
         if self.socket:
             self.socket.close()
-        self.is_connected = False
+            self.socket = None
 
     def send(self, text):
         self.socket.send(text.encode())
@@ -33,6 +23,3 @@ class Client:
     def receive(self):
         data = self.socket.recv(self.max_message_size)
         return data.decode()
-
-    def get_status(self):
-        return self.is_connected
