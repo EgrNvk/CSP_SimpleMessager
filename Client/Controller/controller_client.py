8dfe8b877486a41cc8df007e93e72daad71a54ba
EgrNvk <egrnvk@gmail.com>
2026-02-13 20:45:30 +0200

    Create controller_client

--- Client/Controller/controller_client.py
+++ Client/Controller/controller_client.py
@@ -1,4 +1,48 @@
 import time
 
 class ClientController:
-    def __init__(self,):
+    def __init__(self, view, client):
+        self.view = view
+        self.client = client
+
+        self.view.btn_send.configure(command=self.on_send_click)
+        # self.view.entry.bind("<Return>", self.on_send_click)
+
+        self.running=False
+
+    def start(self):
+        try:
+            self.client.connect()
+        except:
+            self.view.add_line("SYSTEM: Server offline")
+            self.view.btn_send.config(state="disabled")
+            # time.sleep(5)
+            # self.start()
+            self.view.root.after(5000, self.start)
+            return
+
+        self.client.socket.setblocking(False)
+        self.running = True
+        self.view.add_line("SYSTEM: Server online")
+        self.view.btn_send.config(state="normal")
+        self.poll_receive()
+
+    def on_send_click(self):
+        text = self.view.entry.get()
+        if text == "":
+            return
+
+        self.view.add_line("Client: " + text)
+        self.view.entry.delete(0, "end")
+
+        self.client.send(text)
+
+    def poll_receive(self):
+        try:
+            msg=self.client.receive()
+            if msg:
+                self.view.add_line("Server: " + msg)
+        except:
+            pass
+
+        self.view.root.after(50, self.poll_receive)
