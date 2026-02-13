8afca5c6143eafbe8db109bd3e3dd540d03e3328
EgrNvk <egrnvk@gmail.com>
2026-02-13 20:23:43 +0200

    Erased model_server of input/output

--- Server/Model/model_server.py
+++ Server/Model/model_server.py
@@ -4,41 +4,34 @@ class Server:
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.server = None
-        self.conn = None
-        self.addr = None
+
+        self.server_socket = None
+        self.client_socket = None
         self.max_message_size = 1024
-        self.is_running = False
 
     def start(self):
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((self.host, self.port))
-        self.server.listen(1)
-        self.is_running = True
-        print("Server start")
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(1)
 
     def accept_client(self):
-        self.conn, self.addr = self.server.accept()
-        print("Client connected by ", self.addr)
+        self.client_socket, _addr = self.server_socket.accept()
 
-    def receive(self):
-        data = self.conn.recv(self.max_message_size)
-        return data.decode()
-
-    def send(self, text):
-        self.conn.send(str(text).encode())
-
-    def close_client(self):
-        if self.conn:
-            self.conn.close()
-            print(f"Client {self.addr} disconnected")
-        self.conn = None
-        self.addr = None
+    def disconnect_client(self):
+        if self.client_socket:
+            self.client_socket.close()
+            self.client_socket = None
 
     def stop(self):
-        self.close_client()
-        if self.server:
-            self.server.close()
-        self.server = None
-        self.is_running = False
-        print("Server closed")
+        self.disconnect_client()
+        if self.server_socket:
+            self.server_socket.close()
+            self.server_socket = None
+
+    def send(self, text):
+        self.client_socket.send(text.encode())
+
+    def receive(self):
+        data = self.client_socket.recv(self.max_message_size)
+        return data.decode()
